e11e43309254b06b18f8fdb81f87e0b848bf13d0
bcrhbrhcdb <apet2804@mpsedu.org>
2024-10-26 14:40:29 -0500

    building area design will show when i implement em

---
+++ javascript/game/codes.js
@@ -0,0 +1,5 @@
+// game codes that give boosts, what do you expect
+
+export const codes = {
+    
+}
---
+++ javascript/game/stats.js
@@ -0,0 +1,18 @@
+// game stats (real)
+
+export const stats = {
+    clicks: 0,
+    totalClicks: 0,
+    allTimeClicks: 0,
+    totalRuns: 0,
+    upgrades: [], //the names of upgrades
+    upgradesOwned: 0,
+    buildings: [], //the names of buildings
+    totalBuildings: 0,
+    amountPerClick: 1,
+    prestige: 0,
+    voids: 0,
+    ascends: 0,
+    usedCheats: false,
+
+}
